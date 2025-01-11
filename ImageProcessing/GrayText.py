c25cea55a71f3e8499e5bd205c7b3840a050c7d4
cactus1386 <cactusinjast@gmail.com>
2025-01-11 23:51:16 +0330

    gray and crop in cv2

---
+++ ImageProcessing/GrayText.py
@@ -0,0 +1,18 @@
+import cv2
+
+img = cv2.imread('./media/House.jpg')
+
+img = cv2.resize(img, (500, 500))
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+loc = (150, 30)
+fontScale = 1
+fontColor = (0, 0, 0)
+cv2.putText(img, "Radin House", loc, font, fontScale,
+            fontColor, lineType=2, thickness=3)
+
+gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+cv2.imshow('Gray', gray)
+cv2.imshow('House', img)
+cv2.waitKey(0)
---
+++ ImageProcessing/crop.py
@@ -0,0 +1,14 @@
+import cv2
+
+img = cv2.imread('./media/football.jpg')
+
+ball = img[393:460, 486:560, :]
+
+img[393:460, 661:735, :] = ball
+
+cv2.rectangle(img, (486, 393), (560, 460), (0, 0, 255), thickness=3)
+
+cv2.imshow('football', img)
+cv2.waitKey(0)
+
+cv2.imwrite('./media/football_ball.jpg', img)
